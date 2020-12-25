a35d205d32077c1643ce6482631995fdb635c098
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-25 04:04:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,11 +7,11 @@ def using_insert(array, element)
 end
 
 def using_uniq(array)
-  array.uniq()
+  array.uniq
 end
 
 def using_flatten(array)
-  array.flatten()
+  array.flatten
 end
 
 def using_delete(array)
