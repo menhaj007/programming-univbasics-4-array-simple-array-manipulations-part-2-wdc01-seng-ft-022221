0a69ae12d397c0947bb301e3c713d7a1946f8e6e
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-25 04:03:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,9 +7,12 @@ def using_insert(array, element)
 end
 
 def using_uniq(array)
-  array.uniq
+  array.uniq()
 end
 
 def using_flatten(array)
-  array.flatten
+  array.flatten()
 end
+
+def using_delete(array)
+  array.delete()
